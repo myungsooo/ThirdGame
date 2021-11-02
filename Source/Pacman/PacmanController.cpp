887bcd2143e0a7a220d3326f5d0bff11f2db8b66
soo <audtn0508@naver.com>
2021-11-02 22:03:21 +0900

    PacmanController C++ 작성

--- Source/Pacman/PacmanController.cpp
+++ Source/Pacman/PacmanController.cpp
@@ -3,3 +3,56 @@
 
 #include "PacmanController.h"
 
+#include "PacmanPawn.h"
+
+APacmanPawn* APacmanController::GetPacmanPawn() const
+{
+	return Cast<APacmanPawn>(GetPawn());
+}
+
+void APacmanController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	InputComponent->BindAction("MoveUp", IE_Pressed, this, &APacmanController::MoveUp);
+	InputComponent->BindAction("MoveDown", IE_Pressed, this, &APacmanController::MoveDown);
+	InputComponent->BindAction("MoveLeft", IE_Pressed, this, &APacmanController::MoveLeft);
+	InputComponent->BindAction("MoveRight", IE_Pressed, this, &APacmanController::MoveRight);
+}
+
+
+void APacmanController::MoveUp()
+{
+
+	if(GetPacmanPawn() != nullptr)
+	{
+		GetPacmanPawn()->SetDirection(FVector::UpVector);
+	}
+}
+
+void APacmanController::MoveDown()
+{
+	
+	if(GetPacmanPawn() != nullptr)
+	{
+		GetPacmanPawn()->SetDirection(FVector::DownVector);
+	}
+}
+
+void APacmanController::MoveLeft()
+{
+
+	if(GetPacmanPawn() != nullptr)
+	{
+		GetPacmanPawn()->SetDirection(FVector::LeftVector);
+	}
+}
+
+void APacmanController::MoveRight()
+{
+
+	if(GetPacmanPawn() != nullptr)
+	{
+		GetPacmanPawn()->SetDirection(FVector::RightVector);
+	}
+}
--- Source/Pacman/PacmanController.h
+++ Source/Pacman/PacmanController.h
@@ -6,12 +6,23 @@
 #include "GameFramework/PlayerController.h"
 #include "PacmanController.generated.h"
 
-/**
- * 
- */
+class APacmanPawn;
+
 UCLASS()
 class PACMAN_API APacmanController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+protected:
+    
+    void SetupInputComponent() override;
+
+	APacmanPawn* GetPacmanPawn() const;
+
+public:
+
+	void MoveUp();
+	void MoveDown();
+	void MoveLeft();
+	void MoveRight();
 };
