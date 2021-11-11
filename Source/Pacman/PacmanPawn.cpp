c50b3a27b1003758b289a3c4a314d444575a5899
soo <audtn0508@naver.com>
2021-11-11 22:12:13 +0900

    PacmanPawn C++작성

--- Source/Pacman/PacmanPawn.cpp
+++ Source/Pacman/PacmanPawn.cpp
@@ -70,5 +70,9 @@ void APacmanPawn::OnOverlapBegin(AActor* PlayerActor, AActor* OtherActor)
 		Cast<AFoodie>(OtherActor)->Consume();
 	}
 	
+	if (OtherActor->ActorHasTag("Foodie.PowerUp"))
+	{
+		Cast<AFoodie>(OtherActor)->Consume();
+	}
 }
 
